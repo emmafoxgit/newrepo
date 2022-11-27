f14a21ca663cfdab9fc3e5ff9bdb4049c3f6ede4
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2022-11-27 20:12:42 +0000

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -20,21 +20,16 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "demo" {
+# Create a resource group
+resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-##  Demo now
-resource "azurerm_storage_account" "StorageAccountDemo" {
-  name                     = "satestwwwwant000012"
-  resource_group_name      = azurerm_resource_group.demo.name
-  location                 = azurerm_resource_group.demo.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    video = "azure"
-    channel = "CloudQuickLabs"
-  }
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
 }
