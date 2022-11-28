30e8441bf3ccfd7d2a52cade07d7e20f353e24ea
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2022-11-28 08:38:35 +0000

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -33,3 +33,22 @@ resource "azurerm_virtual_network" "example" {
   location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
 }
+
+resource "azurerm_resource_group" "demo" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+##  Demo now
+resource "azurerm_storage_account" "StorageAccountDemo" {
+  name                     = "satestssssssssssssant000012"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    video = "azure"
+    channel = "CloudQuickLabs"
+  }
+}
