836ec4a8c79a57cb80bae5425927e2e8bbb1f597
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2022-11-28 08:40:35 +0000

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -41,7 +41,7 @@ resource "azurerm_resource_group" "demo" {
 
 ##  Demo now
 resource "azurerm_storage_account" "StorageAccountDemo" {
-  name                     = "satestssssssssssssant000012"
+  name                     = "satestsssant000012"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
