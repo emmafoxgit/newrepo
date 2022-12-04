93344fd682bae3347f61ae3a738a7ea036efda1a
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2022-12-04 18:25:04 +0000

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -53,7 +53,7 @@ resource "azurerm_app_service_plan" "example" {
 }
 
 resource "azurerm_function_app" "example" {
-  name                       = "test-azure-functions"
+  name                       = "zzterest-azure-functions"
   location                   = azurerm_resource_group.example.location
   resource_group_name        = azurerm_resource_group.example.name
   app_service_plan_id        = azurerm_app_service_plan.example.id
