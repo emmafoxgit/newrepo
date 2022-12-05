1f9e3fb083511eaf922d2a34c68a81e6c69c7156
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2022-12-05 18:26:29 +0000

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -41,76 +41,23 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "LRS"
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "azure-functions-test-service-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+#resource "azurerm_app_service_plan" "example" {
+#  name                = "azure-functions-test-service-plan"
+#  location            = azurerm_resource_group.example.location
+#  resource_group_name = azurerm_resource_group.example.name
 
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
+#  sku {
+#    tier = "Standard"
+#    size = "S1"
+#  }
+#}
 
-resource "azurerm_function_app" "example" {
-  name                       = "zzterest-azure-functions"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
-  app_service_plan_id        = azurerm_app_service_plan.example.id
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-}
+#resource "azurerm_function_app" "example" {
+#  name                       = "zzterest-azure-functions"
+ # location                   = azurerm_resource_group.example.location
+ # resource_group_name        = azurerm_resource_group.example.name
+ # app_service_plan_id        = azurerm_app_service_plan.example.id
+ # storage_account_name       = azurerm_storage_account.example.name
+ # storage_account_access_key = azurerm_storage_account.example.primary_access_key
+#}
 
-
-
-resource "azurerm_resource_group" "qqexample" {
-  name     = "wwertexample-resource-group"
-  location = "West Europe"
-}
-
-resource "random_integer" "ri" {
-  min = 10000
-  max = 99999
-}
-
-resource "azurerm_cosmosdb_account" "db" {
-  name                = "tfex-cosmos-db-${random_integer.ri.result}"
-  location            = azurerm_resource_group.qqexample.location
-  resource_group_name = azurerm_resource_group.qqexample.name
-  offer_type          = "Standard"
-  kind                = "MongoDB"
-
-  enable_automatic_failover = true
-
-  capabilities {
-    name = "EnableAggregationPipeline"
-  }
-
-  capabilities {
-    name = "mongoEnableDocLevelTTL"
-  }
-
-  capabilities {
-    name = "MongoDBv3.4"
-  }
-
-  capabilities {
-    name = "EnableMongo"
-  }
-
-  consistency_policy {
-    consistency_level       = "BoundedStaleness"
-    max_interval_in_seconds = 300
-    max_staleness_prefix    = 100000
-  }
-
-  geo_location {
-    location          = "eastus"
-    failover_priority = 1
-  }
-
-  geo_location {
-    location          = "westus"
-    failover_priority = 0
-  }
-}
