540a2ceb1b88b27d5ad5834b2a943ab334d0729a
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2022-12-05 20:29:08 +0000

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -76,8 +76,8 @@ resource "azurerm_network_security_group" "zzzexample" {
 
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.zzzexample.location
+  resource_group_name = azurerm_resource_group.zzzexample.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
@@ -89,7 +89,7 @@ resource "azurerm_virtual_network" "example" {
   subnet {
     name           = "subnet2"
     address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.example.id
+    security_group = azurerm_network_security_group.zzzexample.id
   }
 
   tags = {
