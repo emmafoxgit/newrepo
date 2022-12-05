4b1e58a2ef92324b18db30159e170bccef7413c3
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2022-12-05 20:19:35 +0000

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -33,13 +33,13 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "zzafunctionsapptestsa"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+#resource "azurerm_storage_account" "example" {
+ # name                     = "zzafunctionsapptestsa"
+#  resource_group_name      = azurerm_resource_group.example.name
+#  location                 = azurerm_resource_group.example.location
+#  account_tier             = "Standard"
+#  account_replication_type = "LRS"
+#}
 
 #resource "azurerm_app_service_plan" "example" {
 #  name                = "azure-functions-test-service-plan"
