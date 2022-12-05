754e00489146e6ae3aec94d169008cc34deba9fd
emmafoxgit <119225742+emmafoxgit@users.noreply.github.com>
2022-12-05 20:25:50 +0000

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -61,3 +61,38 @@ resource "azurerm_resource_group" "example" {
  # storage_account_access_key = azurerm_storage_account.example.primary_access_key
 #}
 
+
+
+resource "azurerm_resource_group" "zzzexample" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_network_security_group" "zzzexample" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.zzzexample.location
+  resource_group_name = azurerm_resource_group.zzzexample.name
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
